96f1932513918740917178575a1d2b275ba18b2f
zawho <zthooper2@gmail.com>
2023-09-18 10:14:28 +0800

    Add basic previous slide func

--- src/index.js
+++ src/index.js
@@ -10,8 +10,6 @@ function setImgDisplay() {
     }
 }
 
-setImgDisplay();
-
 function nextSlide() {
     const imgContainer = document.querySelector('.img-container');
     const imgArr = Array.from(imgContainer.childNodes);
@@ -26,9 +24,26 @@ function nextSlide() {
     }
 }
 
-function setBtnFuncs() {
-    const nextBtn = document.querySelector('#fwd-btn');
-    nextBtn.addEventListener('click', nextSlide);
+function prevSlide() {
+    const imgContainer = document.querySelector('.img-container');
+    const imgArr = Array.from(imgContainer.childNodes);
+    for (let i = 0; i < imgArr.length; i++) {
+        if (imgArr[i].style.display === 'flex' && i > 0) {
+            imgArr[i].style.display = 'none';
+            imgArr[i-=1].style.display = 'flex';
+        } else if (imgArr[i].style.display === 'flex' && i === 0) {
+            imgArr[i].style.display = 'none';
+            imgArr[imgArr.length - 1].style.display = 'flex';
+        }
+    }
 }
 
+function setBtnFuncs() {
+    const nextBtn = document.querySelector('#fwd-btn');
+    const prevBtn = document.querySelector('#back-btn');
+    nextBtn.addEventListener('click', nextSlide);
+    prevBtn.addEventListener('click', prevSlide);
+}
+
+setImgDisplay();
 setBtnFuncs();
